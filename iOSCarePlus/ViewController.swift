77ca57a8a1e5f1d0495263756213a8a16ea7f8b7
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-13 21:03:04 +0900

    swiftlint warning 수정

--- iOSCarePlus/ViewController.swift
+++ iOSCarePlus/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var logoView: UIView!
     @IBOutlet weak var logoViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var backgroundImageViewLeadingConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         logoView.layer.cornerRadius = 15
@@ -19,27 +19,27 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.viewDefault()
-        appearLogoViewAnimation(){ [weak self] in
-            //completion 후 동작하는 메서드
+        self.animationSettingDefault()
+        appearLogoViewAnimation { [weak self] in
+            //completion 에서 수행될 메서드에 대해 파라미터로 넣어줌
             self?.slideBackgroundImageAnimation()
             self?.blinkLogoAnimation()
         }
 
     }
-    private func viewDefault(){
-        //초기화를 계속 해줌 viewDidAppear가 될 때 마다
+    private func animationSettingDefault() {
         logoViewTopConstraint.constant = -200
         backgroundImageViewLeadingConstraint.constant = 0
-        logoView.alpha = 0
+        logoView.alpha = 1
         view.layoutIfNeeded()
     }
-    private func appearLogoViewAnimation(completion: @escaping () -> Void){
-        UIView.animate(withDuration: 0.7, delay: 1, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: []) { [weak self] in
-            //velocity 가속도
-            //damping 얼마나 왔다갔다 할건가
-            //animation 효과 동작 관련 클로저
-           //weak self in 은 나중에 설명 (5주차)
+    private func appearLogoViewAnimation(completion: @escaping () -> Void) {
+        UIView.animate(
+            withDuration: 0.7,
+            delay: 1,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 1,
+            options: []) { [weak self] in
             //로고가 내려오도록 만들기 위해서 시작 로고위치를 화면 밖으로 잡고,
             self?.logoViewTopConstraint.constant = 17
             //바로 화면 갱신해달라는 의미. 제약조건이 바뀐 후 화면 update 를 해주는 함수
@@ -52,13 +52,12 @@ class ViewController: UIViewController {
             completion()
         }
     }
-    private func slideBackgroundImageAnimation(){
-        UIView.animate(withDuration: 10, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse]) {
-            //옵션 = 반복, 자동으로 반대로 이걸 반복
+    private func slideBackgroundImageAnimation() {
+        UIView.animate(withDuration: 5, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse]) {
             [weak self] in
             //이미지가 움직여야하니까 이미지의 왼쪽 제약조건을 -값으로 바꿔주도록한다.
-            self?.backgroundImageViewLeadingConstraint.constant = 800
-            self?.loadViewIfNeeded()
+            self?.backgroundImageViewLeadingConstraint.constant = -800
+            self?.view.layoutIfNeeded()
         }
     }
     private func blinkLogoAnimation() {
